c6bd11df0ac498105a509312df68d18bf832ff3b
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-20 02:12:03 -0300

    Login layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,44 @@
+import "../styles/Login.css";
+
 const Login = () => {
-  return <></>;
+  return (
+    <>
+      <div className="login-outer-container">
+        <main className="login-inner-container">
+          <h1>
+            Mia<span>Play</span>
+          </h1>
+
+          <form className="login-form" name="login-form">
+            <input
+              className="login-input"
+              type="text"
+              name="email"
+              placeholder="email"
+              autoComplete="email"
+            />
+            <input
+              className="login-input"
+              type="password"
+              name="password"
+              placeholder="password"
+              autoComplete="current-password"
+            />
+
+            <div className="login-button-container">
+              <button className="login-button clickable">
+                <i className="bi bi-plus-circle"></i>
+              </button>
+
+              <button className="login-button clickable">
+                <i className="bi bi-check-circle"></i>
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Login;
