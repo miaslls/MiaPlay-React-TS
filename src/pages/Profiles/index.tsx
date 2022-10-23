f223d392a1410d10ff66669609e3747a3c751c99
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-23 20:37:36 -0300

    Profiles layout

--- src/pages/Profiles/index.tsx
+++ src/pages/Profiles/index.tsx
@@ -1,5 +1,85 @@
+import "./Profiles.css";
+import "./ProfileAvatars.css";
+
 const Profiles = () => {
-  return <></>;
+  return (
+    <>
+      <div className="profile-page-container">
+        <header>
+          <h1>
+            Mia<span>Play</span>
+          </h1>
+        </header>
+
+        <main className="profile-selection-container">
+          <div className="profile-selection-row">
+            <h2>select profile</h2>
+          </div>
+
+          <div className="profile-selection-row">
+            <div className="profile-selection-avatar-list-container">
+              <div className="profile-selection-avatar-container">
+                <div className="profile-selection-avatar profile-selection-avatar-1">
+                  {/* <div className="profile-selection-edit-avatar-icon">
+                    <i className="bi bi-pencil"></i>
+                  </div> */}
+                </div>
+                <div className="profile-selection-avatar-name">CAMILA</div>
+              </div>
+
+              <div className="profile-selection-avatar-container">
+                <div className="profile-selection-avatar profile-selection-avatar-2">
+                  {/* <div className="profile-selection-edit-avatar-icon">
+                    <i className="bi bi-pencil"></i>
+                  </div> */}
+                </div>
+                <div className="profile-selection-avatar-name">FULANA</div>
+              </div>
+
+              <div className="profile-selection-avatar-container">
+                <div className="profile-selection-avatar profile-selection-avatar-3">
+                  {/* <div className="profile-selection-edit-avatar-icon">
+                    <i className="bi bi-pencil"></i>
+                  </div> */}
+                </div>
+                <div className="profile-selection-avatar-name">FULANA 2</div>
+              </div>
+
+              <div className="profile-selection-avatar-container">
+                <div className="profile-selection-avatar profile-selection-avatar-4">
+                  {/* <div className="profile-selection-edit-avatar-icon">
+                    <i className="bi bi-pencil"></i>
+                  </div> */}
+                </div>
+                <div className="profile-selection-avatar-name">FULANA 3</div>
+              </div>
+
+              <div className="profile-selection-new">
+                <div className="profile-selection-new-icon clickable">
+                  <i className="bi bi-plus-circle"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="profile-selection-row">
+            <div className="profile-selection-edit-icon clickable">
+              <i className="bi bi-pencil"></i>
+            </div>
+          </div>
+        </main>
+
+        <footer className="profile-page-footer">
+          <h1 className="footer-title">
+            Mia<span>Play</span>
+          </h1>
+          <div className="profile-page-footer-logout clickable">
+            <i className="bi bi-x-circle"></i>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 };
 
 export default Profiles;
