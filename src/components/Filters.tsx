9a44fb7ee2f94eb02affc8230ee96c53aa60a94b
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-23 23:56:58 -0300

    add Filters to GameList

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -3,24 +3,12 @@ import "../styles/Filters.css";
 const Filters = () => {
   return (
     <>
-      <nav className="filters">
-        <div className="genre-menu clickable">
-          <span>all genres</span>
-          <span>
-            <i className="bi bi-chevron-down"></i>
-          </span>
+      <nav className="sort">
+        <div className="sort-item clickable">
+          <i className="bi bi-sort-alpha-down"></i>
         </div>
-
-        <div className="sort">
-          <div className="sort-item clickable">
-            <i className="bi bi-grid"></i>
-          </div>
-          <div className="sort-item clickable">
-            <i className="bi bi-sort-alpha-down"></i>
-          </div>
-          <div className="sort-item sort-item--selected clickable">
-            <i className="bi bi-calendar-date"></i>
-          </div>
+        <div className="sort-item sort-item--selected clickable">
+          <i className="bi bi-calendar-date"></i>
         </div>
       </nav>
     </>
--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,15 +1,22 @@
 import "../styles/GameList.css";
 
 import GameListItem from "./GameListItem";
+import Filters from "./Filters";
 
 const GameList = () => {
   return (
     <>
-      <section className="game-list-genre-container">
+      <section className="game-list-container">
         <div className="game-list-header">
-          <h2 className="genre-title">genre</h2>
-          <div className="game-list-expand-collapse-icon clickable">
-            <i className="bi bi-chevron-double-down"></i>
+          <div className="genre">
+            <h2 className="genre-title">genre</h2>
+            <div className="expand-genre-icon clickable">
+              <i className="bi bi-chevron-double-down"></i>
+            </div>
+          </div>
+
+          <div className="game-list-filter">
+            <Filters />
           </div>
         </div>
         <div className="game-list-items">
