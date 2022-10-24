c735690bab76a93db7956cb01477df5e139f0f64
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-23 23:26:31 -0300

    Header: profile icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,31 @@
 import "../styles/Header.css";
 
+import avatar1 from "../assets/avatars/100/avatar-1.png";
+
 const Header = () => {
   return (
     <>
       <header>
-        <h1>
-          Mia<span>Play</span>
-        </h1>
+        <div className="header-left">
+          <img src={avatar1} alt="profile icon" />
+        </div>
+        <div className="header-right">
+          <h1>
+            Mia<span>Play</span>
+          </h1>
 
-        <nav className="main-menu">
-          <div className="main-menu-item clickable">
-            <i className="bi bi-person"></i>
-          </div>
-          <div className="main-menu-item clickable">
-            <i className="bi bi-sliders"></i>
-          </div>
-          <div className="main-menu-item clickable">
-            <i className="bi bi-house"></i>
-          </div>
-        </nav>
+          <nav className="main-menu">
+            <div className="main-menu-item clickable">
+              <i className="bi bi-person"></i>
+            </div>
+            <div className="main-menu-item clickable">
+              <i className="bi bi-sliders"></i>
+            </div>
+            <div className="main-menu-item clickable">
+              <i className="bi bi-house"></i>
+            </div>
+          </nav>
+        </div>
       </header>
     </>
   );
