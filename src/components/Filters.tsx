5aab7ceb15a9ea36bb7edbc19b0de8ba5ab5ae7e
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-27 09:29:53 -0300

    add sort option (rating)

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -5,6 +5,9 @@ const Filters = () => {
     <>
       <nav className="sort">
         <div className="sort-item clickable">
+          <i className="bi bi-star"></i>
+        </div>
+        <div className="sort-item clickable">
           <i className="bi bi-sort-alpha-down"></i>
         </div>
         <div className="sort-item sort-item--selected clickable">
